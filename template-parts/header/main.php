b72f7e284739d496fd5bf7d198f8babcb2e837c6
Haroon Yamin <haroon.webdev@gmail.com>
2025-05-17 14:17:21 +0500

    Header Banner

--- template-parts/header/main.php
+++ template-parts/header/main.php
@@ -1,6 +1,22 @@
-<div class="bg-(--tester-haroon)">
-    <h2>ADDS</h2>
-</div>
+<?php
+    $banner = get_field('header_banner', 'option');
+    $social_media = $banner['social_media'];
+    $text = $banner['text'];
+
+    $icons = get_field('header_icons', 'option');
+    $account = $icons['account'];
+    $cart = $icons['cart'];
+?>
+
+<section class="bg-primary">
+    <div class="container">
+        <div class="flex flex-row">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+</section>
 
 <header id="header" class="bg-sky-900">
     <div class="container">
