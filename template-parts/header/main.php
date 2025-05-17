a3a6b450b2a4603c7afd8151ab9dfe029a3471fa
Haroon Yamin <haroon.webdev@gmail.com>
2025-05-17 15:12:24 +0500

    Header Banner

--- template-parts/header/main.php
+++ template-parts/header/main.php
@@ -1,7 +1,11 @@
 <?php
+    $global_social_media = get_field('global_social_media', 'option');
+    $facebook = $global_social_media['facebook'];
+    $instagram = $global_social_media['instagram'];
+    $twitter = $global_social_media['twitter'];
+
     $banner = get_field('header_banner', 'option');
     $social_media = $banner['social_media'];
-    $text = $banner['text'];
 
     $icons = get_field('header_icons', 'option');
     $account = $icons['account'];
@@ -9,9 +13,33 @@
 ?>
 
 <section class="bg-primary">
-    <div class="container">
+    <div class="container mx-auto py-1">
         <div class="flex flex-row">
-            <div></div>
+            <?php if( $social_media ) : ?>
+                <div class="flex flex-row gap-1">
+                    <?php foreach( $social_media as $item ) :
+                        if( $item ) : ?>
+                            <?php if( $item === 'fb' ) : ?>
+                                <a href="<?= $facebook; ?>" target="_blank" class="hover:bg-stone-600 rounded">
+                                    <?= get_svg( 'facebook', 'facebook', 'w-24' ); ?>
+                                </a>
+                            <?php endif; ?>
+
+                            <?php if( $item === 'in' ) : ?>
+                                <a href="<?= $instagram; ?>" target="_blank" class="hover:bg-stone-600 rounded">
+                                    <?= get_svg( 'instagram', 'instagram', 'w-24' ); ?>
+                                </a>
+                            <?php endif; ?>
+
+                            <?php if( $item === 'x' ) : ?>
+                                <a href="<?= $instagram; ?>" target="_blank" class="hover:bg-stone-600 rounded">
+                                    <?= get_svg( 'twitter', 'twitter', 'w-24' ); ?>
+                                </a>
+                            <?php endif; ?>
+                        <?php endif;
+                    endforeach; ?>
+                </div>
+            <?php endif; ?>
             <div></div>
             <div></div>
         </div>
