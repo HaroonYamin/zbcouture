ec5353f08cfe4d3324578d5c08d1c02e0ef3d685
Haroon Yamin <haroon.webdev@gmail.com>
2025-05-29 15:01:07 +0500

    footer dynamic fields

--- template-parts/footer/main.php
+++ template-parts/footer/main.php
@@ -1,10 +1,39 @@
 <?php
-    $footer_menu = wp_nav_menu( array( 'theme_location' => 'footer-menu' ) );
+    $site_logo = get_field('site_logo', 'option');
+    $column_2 = get_field('column_2', 'option');
+    $column_3 = get_field('column_3', 'option');
+    $column_4 = get_field('column_4', 'option');
 ?>
 
-<footer id="footer">
+<footer id="footer" class="my-24">
     <div class="container">
-        <?php $footer_menu; ?>
+        <div class="grid grid-cols-12 gap-6">
+            <div class="col-span-3">
+
+            </div>
+
+            <div class="col-span-2">
+                <?php
+                    if( $column_2['heading'] ) {
+                        echo '<h3 class="text-2xl font-bold mb-6">' . $column_2['heading'] . '</h3>';
+                    }
+                    wp_nav_menu( array( 'theme_location' => 'brand-menu' ) ); 
+                ?>
+            </div>
+
+            <div class="col-span-2">
+                <?php
+                    if( $column_2['heading'] ) {
+                        echo '<h3 class="text-2xl font-bold mb-6">' . $column_2['heading'] . '</h3>';
+                    }
+                    wp_nav_menu( array( 'theme_location' => 'service-menu' ) ); 
+                ?>
+            </div>
+
+            <div class="col-span-1"></div>
+
+            <div class="col-span-4"></div>
+        </div>
         <p class="size-16">© 2020. All rights reserved.</p>
     </div>
 </footer>
