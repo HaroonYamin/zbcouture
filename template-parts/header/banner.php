1903fce73866a094a10f0cebf1f5f1dd3a473bd6
Haroon Yamin <haroon.webdev@gmail.com>
2025-06-13 18:17:19 +0500

    #

--- template-parts/header/banner.php
+++ template-parts/header/banner.php
@@ -1,12 +1,12 @@
 <?php
-    $global_social_media = get_field('global_social_media', 'option');
-    $facebook = $global_social_media['facebook'] ?: '';
-    $instagram = $global_social_media['instagram'] ?: false;
-    $twitter = $global_social_media['twitter'];
+    $global_social_media = get_field('global_social_media', 'option') ?: [];
+    $facebook = $global_social_media['facebook'] ?? '';
+    $instagram = $global_social_media['instagram'] ?? '';
+    $twitter = $global_social_media['twitter'] ?? '';
 
-    $banner = get_field('header_banner', 'option');
-    $social_media = $banner['social_media'];
-    $text = $banner['text'];
+    $banner = get_field('header_banner', 'option') ?: [];
+    $social_media = $banner['social_media'] ?? [];
+    $text = $banner['text'] ?? '';
 
 if ($social_media || $text): ?>
 
@@ -17,20 +17,20 @@ if ($social_media || $text): ?>
                     <div class="flex flex-row gap-1">
                         <?php foreach( $social_media as $item ) :
                             if( $item ) : ?>
-                                <?php if( $item === 'fb' ) : ?>
-                                    <a href="<?= $facebook; ?>" target="_blank" class="hover:bg-stone-600 rounded">
+                                <?php if( $item === 'fb' && $facebook ) : ?>
+                                    <a href="<?= esc_url($facebook); ?>" target="_blank" rel="noopener noreferrer" class="hover:bg-stone-600 rounded">
                                         <?= get_svg( 'facebook', 'facebook', 'w-24' ); ?>
                                     </a>
                                 <?php endif; ?>
 
-                                <?php if( $item === 'in' ) : ?>
-                                    <a href="<?= $instagram; ?>" target="_blank" class="hover:bg-stone-600 rounded">
+                                <?php if( $item === 'in' && $instagram ) : ?>
+                                    <a href="<?= esc_url($instagram); ?>" target="_blank" rel="noopener noreferrer" class="hover:bg-stone-600 rounded">
                                         <?= get_svg( 'instagram', 'instagram', 'w-24' ); ?>
                                     </a>
                                 <?php endif; ?>
 
-                                <?php if( $item === 'x' ) : ?>
-                                    <a href="<?= $instagram; ?>" target="_blank" class="hover:bg-stone-600 rounded">
+                                <?php if( $item === 'x' && $twitter ) : ?>
+                                    <a href="<?= esc_url($twitter); ?>" target="_blank" rel="noopener noreferrer" class="hover:bg-stone-600 rounded">
                                         <?= get_svg( 'twitter', 'twitter', 'w-24' ); ?>
                                     </a>
                                 <?php endif; ?>
@@ -41,7 +41,7 @@ if ($social_media || $text): ?>
 
                 <?php if( $text ) : ?>
                     <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                        <p class="text-white font-medium"><?= $text; ?></p>
+                        <p class="text-white font-medium"><?= esc_html($text); ?></p>
                     </div>
                 <?php endif; ?>
 
