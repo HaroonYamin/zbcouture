1845432cfdbc1c8c79daa81714a975537a4528c6
asjadakhtar <asjadtech203@gmail.com>
2025-06-16 15:49:05 +0500

    about

--- php/custom-fields/blocks/stories.php
+++ php/custom-fields/blocks/stories.php
@@ -1,13 +1,5 @@
 <section class="py-16 text-center">
   <div class="container mx-auto px-4">
-    <h2 class="text-[#27221E] sm:text-4xl text-3xl font-primary font-normal mb-6">
-      Hand Painted <span class="italic">Love Stories</span>
-    </h2>
-    <p class="sm:text-lg text-base max-w-2xl	text-[#27221E] leading-relaxed m-auto sm:mb-20 mb-12 font-normal">
-      <span class="font-semibold">ZB</span> Couture features hand-painted, made-to-order gowns for bridal and formal
-      occasions. Each gown offers timeless and classic beauty through Zahra’s intricately
-      hand-painted motifs on luxury fabrics of the highest Divider quality.
-    </p>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-y-12 md:gap-y-0 text-[#27221E]">
       <div class="px-6 md:px-4 md:border-l-0 flex flex-col items-center">
         <h3 class="text-lg sm:text-xl font-medium mb-2">Handmade</h3>
