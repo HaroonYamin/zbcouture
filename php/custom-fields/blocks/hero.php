9372260d25b87259668c60fb1a6afce6628a9158
asjadakhtar <asjadtech203@gmail.com>
2025-06-16 14:21:32 +0500

    Avenir

--- php/custom-fields/blocks/hero.php
+++ php/custom-fields/blocks/hero.php
@@ -7,10 +7,7 @@
             style="background-image: url('https://darkgoldenrod-dinosaur-500813.hostingersite.com/wp-content/uploads/2025/06/banner-image-scaled.jpg');">
         </div>
         <div class="swiper-slide bg-center bg-cover h-full w-full" 
-            style="background-image: url('https://darkgoldenrod-dinosaur-500813.hostingersite.com/wp-content/uploads/2025/06/banner-image-scaled.jpg');">
-        </div>
-        <div class="swiper-slide bg-center bg-cover h-full w-full" 
-            style="background-image: url('https://darkgoldenrod-dinosaur-500813.hostingersite.com/wp-content/uploads/2025/06/banner-image-scaled.jpg');">
+            style="background-image: url('http://localhost/zahrabatool/wp-content/uploads/2025/06/slide-2.jpg');">
         </div>
     </div>
   </div>
@@ -20,21 +17,20 @@
 
   <!-- 3. Foreground Content (Top Layer) -->
   <div class="absolute inset-0 flex flex-col items-center justify-center h-full text-center text-white px-4 z-10">
-      <h1 class="text-[64px] font-primary font-normal max-w-484px lh-normal">
-          You've Never Been
-          <span class="italic">Just a Bride.</span>
+      <h1 class="text-[48px] font-primary font-normal max-w-484px lh-normal">
+          Wear the story only you could inspire
       </h1>
-      <p class="mt-6 text-[20px] font-normal max-w-336px">
-          You've always seen beauty differently. Your gown should, too.
+      <p class="mt-6 text-[20px] font-normal max-w-[490px]">
+          Zahra paints each gown by hand, turning your love, energy, and essence into a once-in-a-lifetime creation.
       </p>
-      <a href="#" class="text-base mt-6 bg-white text-black px-6 py-3 hover:bg-gray-200 transition">
-          Book an Appointment
+      <a href="#" class="text-[16px] font-medium mt-6 bg-transparent border border-white rounded-[12px] text-white px-8 py-[12px] hover:bg-gray-200 transition hover:text-black">
+          Book a Private Consultation
       </a>
   </div>
 
   <!-- Positioned Pagination -->
   <div class="absolute bottom-8 left-1/2 transform -translate-x-1/2 z-10">
-    <div class="banner-pagination custom-pagination"></div>
+    <div class="banner-pagination"></div>
   </div>
   
 </section>
