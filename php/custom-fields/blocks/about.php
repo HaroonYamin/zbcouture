7adaa1a654d7c4ecb2d6bef640ed7f30358acab4
Haroon Yamin <haroon.webdev@gmail.com>
2025-07-19 18:51:03 +0500

    #

--- php/custom-fields/blocks/about.php
+++ php/custom-fields/blocks/about.php
@@ -22,24 +22,34 @@
 
             <div class="xl:w-4/12 lg:w-6/12 w-full lg:order-2 order-2 px-4 sm:px-0">
                 <?php if( $label ) : ?>
-                    <?= $ui->label( $label, 'text-[#6D6D6D] mb-4' ); ?>
+                    <div data-aos="fade-right">
+                        <?= $ui->label( $label, 'text-[#6D6D6D] mb-4' ); ?>
+                    </div>
                 <?php endif; ?>
 
                 <?php if( $heading ) : ?>
-                    <?= $ui->section_heading( $heading, 'sm:mb-8 mb-8 max-w-[600px]' ) ?>
+                    <div data-aos="fade-right" data-aos-delay="200">
+                        <?= $ui->section_heading( $heading, 'sm:mb-8 mb-8 max-w-[600px]' ); ?>
+                    </div>
                 <?php endif; ?>
 
                 <?php if( $first ) : ?>
-                    <?= $ui->paragraph( $first, 'text-[#121212] mt-[18px] max-w-[414px]' ) ?>
+                    <div data-aos="fade-right" data-aos-delay="400">
+                        <?= $ui->paragraph( $first, 'text-[#121212] mt-[18px] max-w-[414px]' ); ?>
+                    </div>
                 <?php endif; ?>
 
                 <?php if( $second ) : ?>
-                    <?= $ui->paragraph( $second, 'text-[#121212] mt-[18px] max-w-[414px]' ) ?>
+                    <div data-aos="fade-right" data-aos-delay="400">
+                        <?= $ui->paragraph( $second, 'text-[#121212] mt-[18px] max-w-[414px]' ); ?>
+                    </div>
                 <?php endif; ?>
 
                 <?php if( $button ) : ?>
                     <div class="sm:mt-11 mt-6">
-                        <?= $ui->black_button( $button['title'], $button['url'], $button['target'] ); ?>
+                        <div data-aos="fade-right" data-aos-delay="600">
+                            <?= $ui->black_button( $button['title'], $button['url'], $button['target'] ); ?>
+                        </div>
                     </div>
                 <?php endif; ?>
             </div>
