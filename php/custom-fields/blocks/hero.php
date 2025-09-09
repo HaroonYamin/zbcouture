3a916821b6f18e4add781138423826d09545fa32
asjadakhtar <asjadtech203@gmail.com>
2025-09-09 16:20:34 +0500

    #

--- php/custom-fields/blocks/hero.php
+++ php/custom-fields/blocks/hero.php
@@ -19,30 +19,80 @@
                     $title = $slide['title'];
                     $paragraph = $slide['paragraph'];
                     $button = $slide['button'];
+                    $content_position = $slide['content_position']; // Get the content position
+
+                    // Determine flexbox classes based on content_position
+                    $justify_class = 'justify-center'; // Default to center
+                    $align_class = 'items-center';     // Default to center
+
+                    switch ($content_position) {
+                        case 'top-left':
+                            $justify_class = 'justify-start';
+                            $align_class = 'items-start';
+                            break;
+                        case 'top-center':
+                            $justify_class = 'justify-start';
+                            $align_class = 'items-center';
+                            break;
+                        case 'top-right':
+                            $justify_class = 'justify-start';
+                            $align_class = 'items-end';
+                            break;
+                        case 'middle-left':
+                            $justify_class = 'justify-center';
+                            $align_class = 'items-start';
+                            break;
+                        case 'middle-center':
+                            $justify_class = 'justify-center';
+                            $align_class = 'items-center';
+                            break;
+                        case 'middle-right':
+                            $justify_class = 'justify-center';
+                            $align_class = 'items-end';
+                            break;
+                        case 'bottom-left':
+                            $justify_class = 'justify-end';
+                            $align_class = 'items-start';
+                            break;
+                        case 'bottom-center':
+                            $justify_class = 'justify-end';
+                            $align_class = 'items-center';
+                            break;
+                        case 'bottom-right':
+                            $justify_class = 'justify-end';
+                            $align_class = 'items-end';
+                            break;
+                        default:
+                            // Fallback for any unhandled case or if field is not set
+                            $justify_class = 'justify-center';
+                            $align_class = 'items-center';
+                            break;
+                    }
             ?>
                     <div class="swiper-slide">
-                        <div class="w-full h-full bg-cover bg-center bg-no-repeat relative" 
+                        <div class="w-full h-full bg-cover bg-center bg-no-repeat relative"
                             <?php if( $image ) {
                                 echo 'style="background-image: url(' . $image . ');"';
-                            } ?> 
+                            } ?>
                             data-aos="fade-in"
                             data-aos-delay="0"
                             data-aos-duration="700">
 
                             <!-- Overlay (Middle Layer) -->
                             <div class="absolute inset-0 bg-black/40 z-10"></div>
-                        
+
                             <!-- Foreground Content (Top Layer) -->
-                            <div class="absolute inset-0 flex flex-col items-center sm:justify-center justify-end h-full text-center text-white px-4 z-20">
+                            <!-- Dynamic classes based on $content_position -->
+                            <div class="absolute inset-0 flex flex-col <?= $justify_class; ?> <?= $align_class; ?> h-full text-center text-white px-4 z-20">
                                 <div class="swiper-banner-content mb-24 sm:mb-0">
                                     <?php if( $title ) : ?>
                                         <?= $ui->main_heading( $title ); ?>
                                     <?php endif; ?>
-    
+
                                     <?php if( $paragraph ) : ?>
                                         <?= $ui->paragraph( $paragraph, 'sm:mt-6 mt-3' ); ?>
                                     <?php endif; ?>
-    
+
                                     <?php if( $button ) : ?>
                                         <?= $ui->button( $button['title'], $button['url'], $button['target'], 'mt-7' ); ?>
                                     <?php endif; ?>
