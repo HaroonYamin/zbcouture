9d72edf7e164fa26865e29975be579b04186da29
asjadakhtar <asjadtech203@gmail.com>
2025-09-23 16:02:47 +0500

    #

--- php/custom-fields/blocks/testimonials.php
+++ php/custom-fields/blocks/testimonials.php
@@ -89,9 +89,12 @@ if( !$enable ) return;
                     <p class="text-base font-medium font-secondary text-[#535353]"><?= $role; ?></p>
                   <?php endif; ?>
                 
-                  <!-- Read More Button -->
+                  <!-- Read More Link -->
                   <div class="mt-5">
-                    <?= $ui->black_button( 'Read More', $permalink ); ?>
+                    <a href="<?= esc_url( $permalink ); ?>" 
+                      class="text-[#27221E] underline transition font-medium hover:opacity-50 duration-300">
+                      Skip the rack
+                    </a>
                   </div>
                 </div>
               </div>
@@ -101,21 +104,19 @@ if( !$enable ) return;
       </div>
 
       <!-- Navigation Arrows - MOVED OUTSIDE SWIPER LOOP -->
-      <div class="flex justify-center gap-6 mt-8">
+      <!-- <div class="flex justify-center gap-6 mt-8">
         <button id="testimonial-arrow-left" class="z-4 hover:opacity-50 transition-all duration-300"> 
-          <!-- SVG Left -->
           <svg width="28px" height="28px" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" clip-rule="evenodd" d="M6.85355 3.14645C7.04882 3.34171 7.04882 3.65829 6.85355 3.85355L3.70711 7H12.5C12.7761 7 13 7.22386 13 7.5C13 7.77614 12.7761 8 12.5 8H3.70711L6.85355 11.1464C7.04882 11.3417 7.04882 11.6583 6.85355 11.8536C6.65829 12.0488 6.34171 12.0488 6.14645 11.8536L2.14645 7.85355C1.95118 7.65829 1.95118 7.34171 2.14645 7.14645L6.14645 3.14645C6.34171 2.95118 6.65829 2.95118 6.85355 3.14645Z" fill="#000000"/>
           </svg>
         </button>
 
         <button id="testimonial-arrow-right" class="z-4 hover:opacity-50 transition-all duration-300"> 
-          <!-- SVG Right -->
           <svg width="28px" height="28px" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" clip-rule="evenodd" d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="#000000"/>
           </svg>
         </button>
-      </div>
+      </div> -->
       
     <?php endif; ?>
 
