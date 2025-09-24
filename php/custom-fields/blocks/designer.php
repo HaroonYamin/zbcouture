584311e195efaf16ccbfad632ba92c24af4f0a74
asjadakhtar <asjadtech203@gmail.com>
2025-09-24 21:29:12 +0500

    #

--- php/custom-fields/blocks/designer.php
+++ php/custom-fields/blocks/designer.php
@@ -28,7 +28,7 @@
     <div class="w-full xl:basis-2/6 text-left">
       <?php if( $label ) : ?>
         <div class="sm:text-sm text-[12px] text-[#6D6D6D] font-secondary mb-6"
-             data-aos="fade-up" data-aos-delay="50">
+             data-aos="fade-left" data-aos-delay="50">
           <?= $ui->label( $label ) ?>
         </div>
       <?php endif; ?>
@@ -40,7 +40,7 @@
 
       <?php if( $sub_heading ) : ?>
         <h3 class="text-2xl text-black leading-[1.43] font-light font-secondary mt-[19px] max-w-[235px] sm:max-w-[100%]"
-            data-aos="fade-up" data-aos-delay="150">
+            data-aos="fade-left" data-aos-delay="150">
           <?= $sub_heading; ?>
         </h3>
       <?php endif; ?>
@@ -55,21 +55,21 @@
       <!-- Paragraphs and Button -->
       <div id="readMoreContainer">
         <?php if( $first ) : ?>
-          <div class="text-[#27221E] font-light leading-[1.3] font-secondary mt-[18px] max-w-[517px]" data-aos="fade-up" data-aos-delay="200">
+          <div class="text-[#27221E] font-light leading-[1.3] font-secondary mt-[18px] max-w-[517px]" data-aos="fade-left" data-aos-delay="200">
             <?= $ui->paragraph( $first ); ?>
           </div>
         <?php endif; ?>
 
         <?php if( $second ) : ?>
-          <div id="secondPara" class="text-[#27221E] font-light leading-[1.3] font-secondary max-w-[517px] truncate-4" data-aos="fade-up" data-aos-delay="250">
+          <div id="secondPara" class="text-[#27221E] font-light leading-[1.3] font-secondary max-w-[517px] truncate-4" data-aos="fade-left" data-aos-delay="250">
             <?= $ui->paragraph( $second ); ?>
           </div>
         <?php endif; ?>
 
 
-        <div data-aos="fade-up" data-aos-delay="300">
+        <div data-aos="fade-left" data-aos-delay="300">
             <div class="btn-aware-wrapper relative inline-block">
-                <button id="toggleBtn" class="btn-aware btn-aware-black relative inline-block px-8 py-3 text-base font-medium border border-black rounded-xl overflow-hidden backdrop-blur-[2px] z-10 cursor-pointer mt-4">
+                <button id="toggleBtn" class="text-base font-medium z-10 cursor-pointer mt-4 hover:underline">
                     <span class="readme-title">Read More</span>
                     <span class="btn-aware-circle"></span>
                 </button>
